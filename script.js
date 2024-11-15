598e9d18a7e7d82ba04aed2c6399bf946a6ba819
Hina Salman <160792823+hinasalman@users.noreply.github.com>
2024-11-15 11:13:51 -0800

    Hackathone-milestone-1

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("toggle-skills").addEventListener("click", function() {
+    const skillsSection = document.getElementById("skills");
+    skillsSection.style.display = skillsSection.style.display === "none" ? "block" : "none";
+});
